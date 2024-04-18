ef394ec6ca89753bb4359eeb52aca424aeed3f9e
marsxnow <raxelportiz@gmail.com>
2024-04-17 19:07:19 -0700

    Starting main following vid camera turns on now

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 image_path = os.path.join('data', 'images')
 number_images = 30
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 for imgnum in range(number_images):
     print('Collection Image {}'.format(imgnum))
     ret, frame = cap.read()
